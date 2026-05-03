cde497db2f1f10e8730d9898612c9fd15ee9e6b9
Vyankatesh Dande <vyankateshdande.vd@gmail.com>
2026-05-03 19:09:58 +0530

    Separated script into stages; added GNOME configs

--- init.sh
+++ init.sh
@@ -1,48 +1,13 @@
-sudo apt update
+#!/usr/bin/env bash
+set -euo pipefail
 
-# install packages
-sudo apt install -y \
-    wget gpg curl \
-    git podman gparted \
-    python3 python3-pip python3-venv \
-    flatpak gnome-software-plugin-flatpak \
-    gnome-shell-extension-dashtodock \
-    gnome-tweaks
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-gnome-extensions enable dash-to-dock@micxgx.gmail.com || true
+. "$SCRIPT_DIR/system-installs.sh"
+. "$SCRIPT_DIR/system-config.sh"
+. "$SCRIPT_DIR/user-installs.sh"
+. "$SCRIPT_DIR/user-config.sh"
 
-# flatpak business
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak update -y
-flatpak install -y md.obsidian.Obsidian
-
-# install node
-curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
-\. "$HOME/.nvm/nvm.sh"
-nvm install --lts
-
-# add microsoft keys
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -D -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft.gpg
-rm microsoft.gpg
-
-# add vs code repo
-sudo tee /etc/apt/sources.list.d/vscode.sources > /dev/null << 'EOF'
-Types: deb
-URIs: https://packages.microsoft.com/repos/code
-Suites: stable
-Components: main
-Architectures: amd64
-Signed-By: /usr/share/keyrings/microsoft.gpg
-EOF
-
-# install vs code
-sudo apt update
-sudo apt install -y code
-
+sudo apt update -y
+sudo apt upgrade -y
 sudo apt autoremove -y
-
-# configure git
-git config --global user.name "Vyankatesh Dande"
-git config --global user.email "vyankateshdande.vd@gmail.com"
-git config --global init.defaultBranch main
---
+++ user-installs.sh
@@ -0,0 +1,14 @@
+echo "[+] Installing apt packages"
+
+sudo apt remove -y firefox-esr || true
+sudo apt install -y gnome-tweaks code firefox
+
+
+echo "[+] Installing Flatpaks"
+
+flatpak install -y flathub md.obsidian.Obsidian
+
+echo "[+] Installing GNOME Extensions"
+
+gext install dash-to-dock@micxgx.gmail.com || true
+gext install gtk4-ding@smedius.gitlab.com || true
